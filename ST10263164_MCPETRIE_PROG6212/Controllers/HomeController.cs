19f681c699e26eb3884fcfa00b83fc3ea3b7534d
gatekeeper321 <131513420+gatekeeper321@users.noreply.github.com>
2024-09-10 12:54:46 +0200

    Add project files.

---
+++ ST10263164_MCPETRIE_PROG6212/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using ST10263164_MCPETRIE_PROG6212.Models;
+using System.Diagnostics;
+
+namespace ST10263164_MCPETRIE_PROG6212.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult PendingClaims()
+        {
+            return View();
+        }
+        public IActionResult CreateClaim()
+        {
+            return View();
+        }
+        public IActionResult MyClaims()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ST10263164_MCPETRIE_PROG6212/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10263164_MCPETRIE_PROG6212.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
